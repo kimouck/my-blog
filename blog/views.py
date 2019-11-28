09a5fabd66bb1b5578b6ed9b7ae77bb5ac753223
hakim <cherifi.kimou@gmail.com>
2019-11-28 14:20:44 +0100

    added views to create/editblog

--- blog/views.py
+++ blog/views.py
@@ -10,26 +10,9 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-
- def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
-if request.method == "POST":
-    [...]
-else:
-    form = PostForm(request.POST)
-
-if form.is_valid():
-    post = form.save(commit=False)
-    post.author = request.user
-    post.published_date = timezone.now()
-    post.save()
-
- return redirect('post_detail',pk=post.pk)
- def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+def post_new(request):
     if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
+        form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
@@ -37,6 +20,10 @@ if form.is_valid():
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
-        form = PostForm(instance=post)
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
-    
+
+ return redirect('post_detail',pk=post.pk)
+ def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_edit.html', {'form': form})
